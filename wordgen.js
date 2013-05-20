33db7ab83264bbe293eb226c97c3e0d0d21530ac
Danny_C <danny.cox@aunalytics.com>
2013-05-20 19:38:40 -0400

    Imporved algorithm significantly.

--- wordgen.js
+++ wordgen.js
@@ -1,23 +1,32 @@
 process.on('message', function(ar){
-  var dict = ar[1], rack = ar[0], j = ar[2];
+  var mdict = ar[1], rack = ar[0], j = ar[2]
   var l = rack.length
-  var p = [];
-  dict.forEach(function(v){
-    var r = rack.split(''), w = v.slice(0, v.length - 5).split('')
-    k = 0;
-    while (w.length > 0) {
-      if ( r.indexOf(w[0]) != -1 ) {
-        var i = r.indexOf( w.shift() );
-        r.splice( i, 1 );
-      }
-      else { 
-        if (k<j) { w.shift(); k++ }
-        else if (k==j) {break}
-        }
-      if ( w.length == 0 ) { 
-        p.push(v)
-      }
+  var p = [],
+  unique = ''
+  for (var i in rack) {
+    if (unique.search(rack[i]) == -1){
+      unique += rack[i]
+    } else {
+      continue;
     }
-  })
+    var dict = mdict[rack[i]]
+    dict.forEach(function(v){
+      var r = rack.split(''), w = v.slice(0, v.length - 5).split('')
+      k = 0;
+      while (w.length > 0) {
+        if ( r.indexOf(w[0]) != -1 ) {
+          var i = r.indexOf( w.shift() );
+          r.splice( i, 1 );
+        }
+        else { 
+          if (k<j) { w.shift(); k++ }
+          else if (k==j) {break}
+          }
+        if ( w.length == 0 ) { 
+          p.push(v)
+        }
+      }
+    })
+  } 
   process.send(p)
 })
