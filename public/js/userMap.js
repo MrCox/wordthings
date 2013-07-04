baea35c1197312184f545dfe3c94c3fddf645c49
Danny_C <danny.cox@aunalytics.com>
2013-07-04 14:29:34 -0400

    Fixed bunch of problems, cleaned up some code

--- public/js/userMap.js
+++ public/js/userMap.js
@@ -13,6 +13,16 @@ attrs.forEach(function(a) {
   };
 });
 
+//converts to percentage
+var x = function(_) {
+  var w = window.innerWidth;
+  return w * (_ / 100);
+};
+var y = function(_) {
+  var h = window.innerHeight;
+  return h * (_ / 100);
+};
+
 //global map object
 map = {};
 clickMap = {};
@@ -21,17 +31,18 @@ var b = window,
   w = b.innerWidth,
   h = b.innerHeight,
   collection = ds('#collection'),
+  positionCollection = ds('#positionCollection'),
   container = ds('#container'),
   logo = ds('#logo'),
+  Map = ds('#map'),
   zoom = ds('#zoom'),
   canvas = ds('#canvas').style('background-color', 'black'),
   controlPanel = ds('#controlPanel'),
-  Map = ds('#map').style('width', '100%'),
   advert = ds('#advert').style('visibility', 'hidden'),
-  containsControlPanel = ds('#lazyFix').transform('translate(0,' + -.005 * h + ')'),
+  containsControlPanel = ds('#lazyFix').transform('translate(0,' + -y(.5) + ')'),
   milkyWay = ds('#milkyWay').height(2500).width(2500),
   scaling = ds('#scaling'),
-  plotRoute = ds('#plotRoute').transform('translate(0,' + .03 * h + ')'),
+  plotRoute = ds('#plotRoute').transform('translate(0,' + y(3) + ')'),
   hide = ds('#hide').on('click', function() { 
     clickMap[ds(this).id()].apply(this, arguments)}),
   title = ds('#title')
@@ -62,13 +73,12 @@ var b = window,
     },
     centered,
     factor = 1,
-    currentScale = h / 2500,
     currentMode = 'examine',
-    controlPanelCoords = function(w, h) {
-      return 'translate(' + -w * .03 + ',' + h * .11 + ')';
+    controlPanelCoords = function() {
+      return 'translate(' + -x(3) + ',' + y(11) + ')';
     },
-    logoCoords = function(w, h) { 
-      return 'translate(' + -w * .03 + ',' + -h * .02 + ')';
+    logoCoords = function() { 
+      return 'translate(' + -x(3)+ ',' + -y(2) + ')';
     };
 
 map.zoomEvents = function(s) { 
@@ -88,18 +98,12 @@ map.zoomEvents = function(s) {
         if (map.factor() < 2)
           map.factor(map.factor() + .2);
       };
-      var w = window.innerWidth, h = window.innerHeight;
-      x = (map.factor() - 1) * w,
-      y = (map.factor() - 1) * h,
-      f = map.factor();
-      ds('#scaling').transition().duration(400)
-        .attr('transform', 'scale(' + f + ')'
-        + 'translate(' + (-x/(2* f)) + ',' + (-y/(2 * f)) + ')');
+      map.centerMap(400);
     });
 };
 
 zoom.append('path')
-  .transform('translate(' + .7*w + ',' + .05 * h + ')')
+  .transform('translate(' + x(7) + ',' + y(5) + ')')
   .attr('d', d3.svg.symbol().type('cross').size(300))
   .class('official')
   .id('more')
@@ -107,19 +111,20 @@ zoom.append('path')
   .call(map.zoomEvents);
 
 zoom.append('path')
-  .transform('translate(' + .7*w + ',' + .1 * h + ')scale(1, .5)')
+  .transform('translate(' + x(70) + ',' + y(10) + ')scale(1, .5)')
   .attr('d', d3.svg.symbol().type('square').size(300))
   .id('less')
   .class('official')
   .style('fill-opacity', .5)
   .call(map.zoomEvents);
 
-map.zoomButtons = function(w, h) { 
+map.zoomButtons = function() { 
+  var h = window.innerHeight, w = window.innerWidth;
   zoom.da('path')
     .transform(function(d, i) { 
       var f = i == 0 ? .05 : .1,
         s = i == 0 ? '' : 'scale(1, .5)';
-      return 'translate(' + .7*w + ',' + f * h + ')' + s;
+      return 'translate(' + x(70) + ',' + f * h + ')' + s;
     });
 };
 
@@ -130,18 +135,16 @@ map.factor = function(_) {
   return factor;
 };
 map.currentScale = function(_) {
-  if (!arguments.length) return currentScale;
-  currentScale = _;
-  return currentScale;
+  return window.innerHeight / 2500;
 };
 map.currentMode = function(_) {
   if (!arguments.length) return currentMode;
   currentMode = _;
   return currentMode;
 };
-map.rowHeight = function(h) { return h * .08; };
-map.buttonHeight = function(h) { return h * .03;};
-map.buttonWidth = function(w) { return w * .06; };
+map.rowHeight = function(h) { return y(8); };
+map.buttonHeight = function(h) { return y(4);};
+map.buttonWidth = function(w) { return x(7); };
 
 //data
 map.links = JSON.parse(GTA_data.links);
@@ -190,25 +193,43 @@ map.findAccessibleNodes = function(data) {
 map.updateCollectionScale = function() { 
 };
 
+map.centerMap = function(dur) {
+  var w = window.innerWidth, h = window.innerHeight,
+  k = map.currentScale()*map.factor(),
+  width = k*2500,
+  height = width,
+  offset = x(2.5),
+  newx = w / 2 - width / 2 - offset,
+  newy = h / 2 - height / 2,
+  dur = dur ? dur : 0;
+
+  collection.transition().duration(dur)
+    .attr('transform', 'scale(' + k + ')');
+    
+  positionCollection.transition()
+    .duration(dur)
+    .attr('transform', 'translate(' + newx + ',' + newy + ')');
+};
+
 map.applyScale = function() { 
-  w = window.innerWidth;
-  h = window.innerHeight;
+  var scale = map.currentScale() * map.factor(),
+    w = window.innerWidth, h = window.innerHeight,
+    width = map.currentScale() * 2500,
+    offset = x(2.5),
+    center = w / 2 - width / 2 - offset;
 
-  var scale = map.currentScale((h / 2500) * map.factor()),
-    center = 2500 / scale;
+  map.zoomButtons();
 
-  map.zoomButtons(w, h);
+  map.scaleLogo();
 
-  container.transform(controlPanelCoords(w, h));
+  container.transform(controlPanelCoords());
 
-  logo.transform(logoCoords(w, h));
+  logo.transform(logoCoords());
 
-  canvas.attr('height', h)
-    .attr('width', w)
+  canvas.attr('height', h * map.factor())
+    .attr('width', w * map.factor());
 
-  collection
-    .attr('transform', 'scale(' + scale + ')' + 
-    'translate(' + (center - 2500)/4 + ',0)');
+  map.centerMap();
 };
 
 map.scooch = function(critInd, center, width, margin) {
@@ -225,28 +246,23 @@ map.scooch = function(critInd, center, width, margin) {
 };
 
 map.zoom = function(d) {
-  var x, y, k = map.currentScale() * map.factor();
+  var nx, ny, k = map.currentScale();
   var w = window.innerWidth, h = window.innerHeight;
   if (d.name != centered) {
     k = 1.8 * k;
-    x = d.ox * k;
-    y = d.oy * k;
+    nx = d.ox * k;
+    ny = d.oy * k;
     centered = d.name;
     collection.transition()
       .duration(1000)
-      .attr('transform','scale(' + k + ')' 
-        + 'translate(' + (w / 2 - x) + ',' + (h / 2 - y) + ')');
-  } else {
-    k = map.currentScale();
-    center = 2500 / k;
-    centered = null;
-    collection.transition()
+      .attr('transform','scale(' + k + ')');
+    positionCollection.transition()
       .duration(1000)
-      .attr('transform',
-        'scale(' + k + ')' + 
-        'translate(' + (center - 2500) / 4 + ',0)'
-      );
-  }
+      .attr('transform', 'translate(' + (w / 2 - nx) + ',' + (h / 2 - ny) + ')');
+  } else {
+    centered = null;
+    map.centerMap(1000);
+  };
 };
 
 map.buttonEvents = function(s) { 
@@ -262,6 +278,18 @@ map.buttonEvents = function(s) {
 };
 
 // rendering functions
+map.scaleLogo = function() {
+  var titles = title.da('text')[0];
+  ds(titles[0]).x(x(50)).y(y(5));
+  ds(titles[1]).x(x(35 / 2)).y(y(8 / 1.5))
+    .transform('scale(2, 1.5)')
+  ds(titles[2]).x(x(25)).y(y(8))
+    .transform('scale(2,1)');
+  ds(titles[3]).x(x(65 / 2)).y(y(8 / 1.5))
+    .transform('scale(2, 1.5)');
+  title.ds('rect').x(x(34.5)).y(y(8.5))
+    .width(x(31)).height(y(.25));
+};
 map.renderControlPanel = function() {
   var dats = map.buttonData,
     rows = [dats, []];
@@ -371,9 +399,9 @@ map.renderNodes = function(array) {
 map.button = function(d, i) { 
   var w = window.innerWidth,
     h = window.innerHeight,
-    height = map.buttonHeight(h),
-    width = map.buttonWidth(w),
-    rheight = map.rowHeight(h) / 2,
+    height = map.buttonHeight(),
+    width = map.buttonWidth(),
+    rheight = map.rowHeight() / 2,
     critInd = (d.length - 1) / 2,
     center = (w - width) / 2,
     scooch = map.scooch(critInd, center, width);
@@ -406,11 +434,12 @@ map.button = function(d, i) {
     .attr('height', height)
     .attr('width', width)
  
-  newButtons.append('text')
+  var o = newButtons.append('text')
+    .attr('textLength', (width * .8))
     .class('buttonText')
     .attr('dy', '.3em')
     .transform('translate(' + width / 2 + ',' + height / 2 + ')')
-    .text(function(d) {return d.text});
+    .text(function(d) {return d.text})
 
   //exiting buttons
   buttons.exit().remove();
@@ -420,9 +449,9 @@ map.plotRoute = function() {
   var w = window.innerWidth,
     h = window.innerHeight,
     data = map.vertices,
-    rheight = .05 * h / 2,
+    rheight = y(2.5),
     critInd = (data.length - 1) / 2,
-    width = .05 * w,
+    width = x(5),
     center = (w - width) / 2,
     scooch = map.scooch(critInd, center, width);
 
@@ -601,8 +630,8 @@ clickMap.planTrip = function(d, i) {
   if (plotRoute.ds('.background').empty()) {
     plotRoute.append('rect')
       .class('background')
-      .width('100%').height(0)
-      .transition().attr('height', '8%');
+      .width(x(100)).height(0)
+      .transition().attr('height', y(8));
   };
 
   plotRoute.each(function(d, i) {
@@ -639,9 +668,9 @@ map.nodeEvents.click = function(d, i) {
       } else {
         var h = window.innerHeight,
           w = window.innerWidth,
-          rheight = .14 * h / 2;
+          rheight = y(7);
         var instruct = plotRoute.append('text')
-          .transform('translate(' + w / 2 + ',' + rheight + ')')
+          .transform('translate(' + x(50) + ',' + rheight + ')')
           .class('instructions')
           .style('opacity', 0);
         instruct.transition()
